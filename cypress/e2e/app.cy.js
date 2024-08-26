d7ff92b5c83db4b8663b35e8218783b8cde26c01
matosgabriel-leaf <gabriel.matos@withleaf.io>
2024-08-26 20:42:20 -0300

    fix: adjust to proper behavior

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -26,7 +26,8 @@ describe('Image Registration', () => {
 
   const registerForm = new RegisterForm();
   const colors = {
-    errors: 'rgb(220, 53, 69)'
+    errors: 'rgb(220, 53, 69)',
+    success: 'rgb(222, 226, 230)'
   }
 
   describe('Submitting an image with invalid inputs', () => {
@@ -70,5 +71,70 @@ describe('Image Registration', () => {
         assert.strictEqual(border_color, colors.errors)
       })
     })
+  })
+
+  describe('Submitting an image with valid inputs using enter key', () => {
+
+    const input = {
+      title: "puppy",
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWc39Z8jQRTl5HmvTtiU9IU_LkRtf4Ln_zkQ&s",
+
+    }
+
+    after(() => {
+      cy.clearLocalStorage()
+    })
+
+    it('Given I am on the image registration page', () => {
+      cy.visit('/')
+    })
+
+    it(`When I enter "${input.title}" in the title field`, () => {
+      registerForm.typeTitle(input.title)
+    })
+
+    it(`Then I enter "${input.url}" in the URL field`, () => {
+      registerForm.typeUrl(input.url)
+    })
+
+    it('I should see a check icon in the imageUrl field', () => {
+      registerForm.elements.titleInput().should(([$input]) => {
+        const styles = window.getComputedStyle($input);
+        const border = styles.getPropertyValue('border-right-color')
+        assert.strictEqual(border, colors.success)
+      })
+    })
+
+    it(`Then I click the submit button`, () => {
+      registerForm.clickSubmit()
+    })
+
+    it('And the list of registered images should be updated with the new item', () => {
+      cy.get('#card-list .card-img').should((elements) => {
+        const lastElement = elements[elements.length - 1]
+        const src = lastElement.getAttribute('src')
+        assert.strictEqual(src, input.url)
+      })
+    });
+
+    it('And the new item should be stored in the localStorage', () => {
+      cy.getAllLocalStorage().should((ls) => {
+        const currentLs = ls[window.location.origin]
+        const elements = JSON.parse(Object.values(currentLs))
+        const lastElement = elements[elements.length - 1]
+
+        assert.deepStrictEqual(lastElement, {
+          title: input.title,
+          imageUrl: input.url,
+        })
+
+      })
+    })
+
+    it('Then The inputs should be cleared', () => {
+      registerForm.elements.titleInput().should('have.value', '')
+      registerForm.elements.imageUrlInput().should('have.value', '')
+    })
+
   })
 })
